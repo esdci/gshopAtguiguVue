8457f59039b7667bf5e4cb4d4543329e0253f55a
zhangquanjunchn <zhangquanjunchn@163.com>
2021-04-14 10:47:31 +0800

    P85 各导航路由组件（静态）half

---
+++ src/pages/Order/Order.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>OOOOOO</p>
+  </div>
+</template>
+
+<script>
+
+export default {}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
+
---
+++ src/pages/Search/Search.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>SSSSSS</p>
+  </div>
+</template>
+
+<script>
+
+export default {}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
+
